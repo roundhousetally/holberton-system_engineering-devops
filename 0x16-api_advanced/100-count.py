6a402763be6e12f106511304aa78593def882275
roundhousetally <tahlia.w.roper@gmail.com>
2020-09-18 02:43:25 +0000

    exec

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" getting subs from reddit """
+import requests
+
+
+def count_words(subreddit, word_list):
+    try:
+        count_words(subreddit, word_list)
+
+    except Exception:
+        return None
