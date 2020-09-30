b047b2c6c63e1ad0c6a154a13d8d10fec180e3be
Roundhousetally <tahlia.w.roper@gmail.com>
2020-09-30 14:56:25 +0000

    passes puppet lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix typos in php
 
 exec { 'fix':
-     command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-     path => '/bin'
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+  path    => '/bin'
 }
