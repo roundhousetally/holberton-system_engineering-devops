01fce3ce99ffeaee10f23eb36127f95b7938875d
Roundhousetally <tahlia.w.roper@gmail.com>
2020-10-09 18:22:42 +0000

    task 0 fixed

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # change open file limit
 
 exec { 'fix':
-  command => "sed -i 's/15/2000/g' /etc/default/nginx",
-  path    => '/bin'
+        command  => "sed -i 's/15/3000/g' /etc/default/nginx; service nginx restart",
+        path     => ['/bin', '/usr/bin']
 }
